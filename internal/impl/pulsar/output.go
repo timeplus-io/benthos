865092662155be81eba93dd3b0ae61fe0fca3b56
Calvin Zhang <calvin.zhang@timeplus.io>
2023-11-22 09:52:31 -0800

    feat(pulsar output): supported BatchWrite

--- internal/impl/pulsar/output.go
+++ internal/impl/pulsar/output.go
@@ -14,33 +14,7 @@ import (
 func init() {
 	err := service.RegisterOutput(
 		"pulsar",
-		service.NewConfigSpec().
-			Version("3.43.0").
-			Categories("Services").
-			Summary("Write messages to an Apache Pulsar server.").
-			Field(service.NewURLField("url").
-				Description("A URL to connect to.").
-				Example("pulsar://localhost:6650").
-				Example("pulsar://pulsar.us-west.example.com:6650").
-				Example("pulsar+ssl://pulsar.us-west.example.com:6651")).
-			Field(service.NewStringField("topic").
-				Description("The topic to publish to.")).
-			Field(service.NewObjectField("tls",
-				service.NewStringField("root_cas_file").
-					Description("An optional path of a root certificate authority file to use. This is a file, often with a .pem extension, containing a certificate chain from the parent trusted root certificate, to possible intermediate signing certificates, to the host certificate.").
-					Default("").
-					Example("./root_cas.pem")).
-				Description("Specify the path to a custom CA certificate to trust broker TLS service.")).
-			Field(service.NewInterpolatedStringField("key").
-				Description("The key to publish messages with.").
-				Default("")).
-			Field(service.NewInterpolatedStringField("ordering_key").
-				Description("The ordering key to publish messages with.").
-				Default("")).
-			Field(service.NewIntField("max_in_flight").
-				Description("The maximum number of messages to have in flight at a given time. Increase this to improve throughput.").
-				Default(64)).
-			Field(authField()),
+		pulsarSepc(),
 		func(conf *service.ParsedConfig, mgr *service.Resources) (service.Output, int, error) {
 			w, err := newPulsarWriterFromParsed(conf, mgr.Logger())
 			if err != nil {
@@ -55,6 +29,45 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func pulsarSepc() *service.ConfigSpec {
+	return service.NewConfigSpec().
+		Version("3.43.0").
+		Categories("Services").
+		Summary("Write messages to an Apache Pulsar server.").
+		Field(service.NewURLField("url").
+			Description("A URL to connect to.").
+			Example("pulsar://localhost:6650").
+			Example("pulsar://pulsar.us-west.example.com:6650").
+			Example("pulsar+ssl://pulsar.us-west.example.com:6651")).
+		Field(service.NewStringField("topic").
+			Description("The topic to publish to.")).
+		Field(service.NewObjectField("tls",
+			service.NewStringField("root_cas_file").
+				Description("An optional path of a root certificate authority file to use. This is a file, often with a .pem extension, containing a certificate chain from the parent trusted root certificate, to possible intermediate signing certificates, to the host certificate.").
+				Default("").
+				Example("./root_cas.pem")).
+			Description("Specify the path to a custom CA certificate to trust broker TLS service.")).
+		Field(service.NewInterpolatedStringField("key").
+			Description("The key to publish messages with.").
+			Default("")).
+		Field(service.NewInterpolatedStringField("ordering_key").
+			Description("The ordering key to publish messages with.").
+			Default("")).
+		Field(service.NewIntField("max_in_flight").
+			Description("The maximum number of messages to have in flight at a given time. Increase this to improve throughput.").
+			Default(64)).
+		Field(service.NewIntField("batching_max_publish_delay").
+			Description("The time period within which the messages sent will be batched in millisecond").
+			Default(10)).
+		Field(service.NewIntField("batching_max_messages").
+			Description("The maximum number of messages permitted in a batch").
+			Default(1000)).
+		Field(service.NewIntField("batching_max_size").
+			Description("The maximum number of bytes permitted in a batch").
+			Default(128 * 1024)).
+		Field(authField())
 }
 
 //------------------------------------------------------------------------------
@@ -72,6 +85,11 @@ type pulsarWriter struct {
 	rootCasFile string
 	key         *service.InterpolatedString
 	orderingKey *service.InterpolatedString
+
+	// batching
+	batchingMaxPublishDelay time.Duration
+	batchingMaxMessages     int
+	batchingMaxSize         int
 }
 
 func newPulsarWriterFromParsed(conf *service.ParsedConfig, log *service.Logger) (p *pulsarWriter, err error) {
@@ -98,6 +116,19 @@ func newPulsarWriterFromParsed(conf *service.ParsedConfig, log *service.Logger) 
 	if p.orderingKey, err = conf.FieldInterpolatedString("ordering_key"); err != nil {
 		return
 	}
+	if p.batchingMaxMessages, err = conf.FieldInt("batching_max_messages"); err != nil {
+		return
+	}
+	if p.batchingMaxSize, err = conf.FieldInt("batching_max_size"); err != nil {
+		return
+	}
+
+	batchingPeriod, err := conf.FieldInt("batching_max_publish_delay")
+	if err != nil {
+		return
+	}
+	p.batchingMaxPublishDelay = time.Duration(batchingPeriod) * time.Millisecond
+
 	return
 }
 
@@ -135,7 +166,10 @@ func (p *pulsarWriter) Connect(ctx context.Context) error {
 	}
 
 	if producer, err = client.CreateProducer(pulsar.ProducerOptions{
-		Topic: p.topic,
+		BatchingMaxPublishDelay: p.batchingMaxPublishDelay,
+		BatchingMaxMessages:     uint(p.batchingMaxMessages),
+		BatchingMaxSize:         uint(p.batchingMaxSize),
+		Topic:                   p.topic,
 	}); err != nil {
 		client.Close()
 		return err
@@ -196,6 +230,57 @@ func (p *pulsarWriter) Write(ctx context.Context, msg *service.Message) error {
 
 	_, err = r.Send(context.Background(), m)
 	return err
+}
+
+func (p *pulsarWriter) WriteBatch(ctx context.Context, batch service.MessageBatch) error {
+	var r pulsar.Producer
+	p.m.RLock()
+	if p.producer != nil {
+		r = p.producer
+	}
+	p.m.RUnlock()
+
+	if r == nil {
+		return component.ErrNotConnected
+	}
+
+	msgs := make([]*pulsar.ProducerMessage, len(batch))
+
+	err := batch.WalkWithBatchedErrors(func(i int, msg *service.Message) error {
+		b, err := msg.AsBytes()
+		if err != nil {
+			return err
+		}
+
+		m := &pulsar.ProducerMessage{
+			Payload: b,
+		}
+		if key := p.key.Bytes(msg); len(key) > 0 {
+			m.Key = string(key)
+		}
+		if orderingKey := p.orderingKey.Bytes(msg); len(orderingKey) > 0 {
+			m.OrderingKey = string(orderingKey)
+		}
+
+		msgs[i] = m
+
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	// TODO: Need to figure out whether we need to handle error individual in callback
+	// or we can just rely on the error returned by the `Flush`
+	for _, m := range msgs {
+		r.SendAsync(context.Background(), m, callback)
+	}
+
+	return p.producer.Flush()
+}
+
+func callback(id pulsar.MessageID, m *pulsar.ProducerMessage, err error) {
 }
 
 func (p *pulsarWriter) Close(ctx context.Context) error {
